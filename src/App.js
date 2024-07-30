cba5ed900986ae8e458133768563cb4f99a1babb
Vladyslav Kramarenko <vkramarenko.at.work@gmail.com>
2024-07-30 03:29:05 +0100

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter,  Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import NewsFeed from './NewsFeed';
 import Article from './Article';
 import CustomDropdown from './CustomDropdown';
@@ -8,19 +8,19 @@ import './App.css';
 
 function App() {
   return (
-    <ArticlesProvider>
-      <HashRouter>
-        <div className="App">
-          <header>
-            <CustomDropdown />
-          </header>
-          <Routes>
-            <Route path="/" element={<NewsFeed />} />
-            <Route path="/article/:id" element={<Article />} />
-          </Routes>
-        </div>
-      </HashRouter>
-    </ArticlesProvider>
+      <ArticlesProvider>
+        <HashRouter>
+          <div className="App">
+            <header>
+              <CustomDropdown />
+            </header>
+            <Routes>
+              <Route path="/" element={<NewsFeed />} />
+              <Route path="/article/:id" element={<Article />} />
+            </Routes>
+          </div>
+        </HashRouter>
+      </ArticlesProvider>
   );
 }
 
